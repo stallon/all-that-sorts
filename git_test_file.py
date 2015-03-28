7ee08535a7043a20bf4a15ab2e733f47467d9270
Hong SeongBong <stallon72@naver.com>
2015-03-28 23:27:19 +0900

    hotfix is committed.

--- git_test_file.py
+++ git_test_file.py
@@ -4,3 +4,6 @@ print("Hello, My home!!")
 
 # here is a change for branch review_git_test_file.
 print("This is a second change in review-git-test-file")
+
+# This is a hotfix line.
+print("This is a hotfix code")
